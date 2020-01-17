68159efa923ee7cc3b481e0774b74c9ba2272f93
Dipak Acharya <dpakach@gmail.com>
2020-01-17 09:06:29 +0545

    Call correct method of authContext from feature context

--- tests/acceptance/features/bootstrap/FeatureContext.php
+++ tests/acceptance/features/bootstrap/FeatureContext.php
@@ -2594,7 +2594,7 @@ class FeatureContext extends BehatVariablesContext {
 		$this->ocsContext = new OCSContext();
 		$this->authContext = new AuthContext();
 		$this->ocsContext->before($scope);
-		$this->authContext->before($scope);
+		$this->authContext->setUpScenario();
 		$environment->registerContext($this->ocsContext);
 		$environment->registerContext($this->authContext);
 	}
